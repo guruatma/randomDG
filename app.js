03160fe61fb7a786f8a50bbb6cdd96ffa8413688
Alec Singer <51802911+Sosenteam@users.noreply.github.com>
2024-06-26 13:26:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const randomStringElement = document.getElementById('randomString');
+const container = document.getElementById('container');
 
 function getRandomString() {
     const randomIndex = Math.floor(Math.random() * strings.length);
     return strings[randomIndex];
 }
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function updateRandomString() {
     const string = getRandomString();
     randomStringElement.textContent = string;
+    container.style.backgroundColor = getRandomColor();
     fitStringToScreen();
 }
 
 function fitStringToScreen() {
-    const container = document.getElementById('container');
     let fontSize = 100;
     randomStringElement.style.fontSize = `${fontSize}px`;
 
