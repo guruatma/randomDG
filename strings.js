390a230229a1551950223c0cb7d8899aca02bc13
Alec Singer <51802911+Sosenteam@users.noreply.github.com>
2024-06-26 13:12:38 -0700

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,12 @@
+const strings = [
+    "Hello, World!",
+    "Random String Generator",
+    "Responsive Design",
+    "JavaScript is Fun",
+    "Web Development",
+    "Mobile-Friendly",
+    "Coding Challenge",
+    "Array of Strings",
+    "Fit to Screen",
+    "Adaptive Layout"
+];
